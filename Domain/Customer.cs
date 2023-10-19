a6c0e85e22d0acbd90dfc4c4f4c7a0d8c69f7a6b
Tshepo Monene <tmonene@SINGULAR.CO.ZA>
2023-10-19 16:37:21 +0200

    Adding Models

---
+++ Domain/Customer.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Customer
+    {
+        public Guid CustomerID;
+        public string FirstName;
+        public string LastName;
+        public string AddressType;
+        public string City;
+        public string Surburb;
+        public string StreetName;
+        public int PostalCode;
+
+    }
+}
